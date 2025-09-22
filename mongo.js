156e20c76241c09a72512309f626921bb46f1e1a
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-09-22 23:32:35 +0300

    Added exercise 3.19, 3.21, 3.22 modifications

--- mongo.js
+++ mongo.js
@@ -6,8 +6,8 @@ if (process.argv.length < 3) {
 }
 
 if (process.argv.length > 3 && process.argv.length < 5) {
-    console.log('add a new contact in this form (node mongo.js password name number)')
-    process.exit(1)
+  console.log('add a new contact in this form (node mongo.js password name number)')
+  process.exit(1)
 }
 const password = process.argv[2]
 
@@ -17,33 +17,41 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-name: String,
-number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 1,
+    required: true
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length == 3) {
-    console.log('Phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log('Phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length == 5) {
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
+  const person = new Person({
     name: name,
     number: number
-    })
+  })
 
-    person.save().then(result => {
+  person.save().then(result => {
     console.log('Added', name, 'Number', number, 'to Phonebook')
     mongoose.connection.close()
-    })
+  })
 }
